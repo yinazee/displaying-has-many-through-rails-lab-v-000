bbe89c744018496099d48e11fc3610c8e0245042
Yina Zhu <yinagit@gmail.com>
2018-08-08 18:12:31 +0000

    automatically backed up by learn

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,11 +1,6 @@
 class Patient < ActiveRecord::Base
-<<<<<<< HEAD
+
   has_many :appointments
   has_many :doctors, through: :appointments
 
-
-=======
-  has_many :doctors, through: :appointments
-  has_many :appointments
->>>>>>> 1d5a0de0b9319d6bf5252f84dd652e81ef6430a9
 end
