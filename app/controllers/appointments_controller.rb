7540d137676fd88fa56c6fe612ebec8a43b50e85
Yina Zhu <yinagit@gmail.com>
2018-08-08 14:57:17 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,5 @@
 class AppointmentsController < ApplicationController
-<<<<<<< HEAD
+
   def new
   end
 
@@ -7,11 +7,11 @@ class AppointmentsController < ApplicationController
   end
 
   def show
-    @patient = Patient.find{params[:id]}
-    @doctor = Doctor.find{params[:id]}
-    @appointment = Appointment.find{params[:id]}
+    @patient = Patient.find(params[:id])
+    @doctor = Doctor.find(params[:id])
+    @appointment = Appointment.find(params[:id])
   end
-=======
+
 
     def show
       @doctor = Doctor.find(params[:id])
@@ -33,5 +33,4 @@ class AppointmentsController < ApplicationController
 
     end
 
->>>>>>> 1d5a0de0b9319d6bf5252f84dd652e81ef6430a9
 end
